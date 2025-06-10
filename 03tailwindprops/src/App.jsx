cae3312e9aea821a9cd7eef495e50207e2e03c7d
Priyanka-saw <priyankasaw51@gmail.com>
2025-06-10 14:18:55 +0530

    initial commit

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -1,35 +1,42 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
 import Card from './components/card'
+import Navbar from './components/navbar'
+
+
 
 function App() {
   const [count, setCount] = useState(0)
 
-  let myObj = {
-    username : "priyanka",
-    name : "riya"
-  }
+  // let myObj = {
+  //   username : "priyanka",
+  //   name : "riya"
+  // }
 
-  let newArr = {
-    sub : "cs",
-    class: 2,
+  // let newArr = {
+  //   sub : "cs",
+  //   class: 2,
 
-    sub : {
-      name : "niya",
-      subname : "tiya"
-    }
-  }
+  //   sub : {
+  //     name : "niya",
+  //     subname : "tiya"
+  //   }
+  // }
 
 
   return ( 
     <>
-     <h1 className='bg-red-500 text-black p-4 rounded 20px mb-5'>vite and tailwind</h1>
+     {/* <h1 className='bg-red-500 text-black p-4 rounded 20px mb-5'>vite and tailwind</h1> */}
 
-    {/* passing props -> property */}
-      <Card chennal="chaiorcode" name = "Priyanka" msgtext = "Contact me" />
-      <Card name = "nancy" msgtext="et in touch"/>
+
+    {/* passing props -> property*/}
+
+      {/* <Card chennal="chaiorcode" name = "Priyanka" msgtext = "Contact me" />
+      <Card name = "nancy" msgtext="et in touch"/>  */}
+
+        <Navbar />
+
     </>
   )
 }
