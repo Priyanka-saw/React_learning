5d40035ab9bafbd1d18316f529b820103213fb01
Priyanka-saw <priyankasaw51@gmail.com>
2025-06-10 14:18:18 +0530

    navbar

---
+++ 03tailwindprops/src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+
+function Navbar() {
+
+  return (
+
+    <div>
+        <ul type="none">
+            <li>Home</li>
+            <li>About</li>
+            <li>Survices</li>
+            <li>Contact us</li>
+        </ul>
+    </div>
+
+  )
+}
+
+export default Navbar
